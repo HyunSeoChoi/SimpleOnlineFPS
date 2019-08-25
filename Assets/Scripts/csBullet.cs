77458091575cbbc279a8ee6302884c64855ee731
HyunSeoChoi <arenaofjagal@naver.com>
2019-08-25 17:05:05 +0900

    Make Enemy Spawn Randomly

--- Assets/Scripts/csBullet.cs
+++ Assets/Scripts/csBullet.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class csBullet : MonoBehaviour {
+    [HideInInspector]
+    public GameObject playerFrom;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<csHealth>();
+
+        if (health != null)
+        {
+            health.TakeDamage(playerFrom, 10);
+        }
+
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/csEnemySpawner.cs
+++ Assets/Scripts/csEnemySpawner.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class csEnemySpawner : MonoBehaviour {
 
+    public GameObject enemy;
+    public GameObject spawnPoint;
+    public int numbetOfEnemies;
+
+    [HideInInspector]
+    public List<csSpawnPoint> enemySpawnPoints;
+
 	// Use this for initialization
 	void Start () {
-		
+	    for(int i=0; i<numbetOfEnemies; i++)
+        {
+            var spawnPosition = new Vector3(Random.Range(-8f, 8f), 0f, Random.Range(-8f, 8f));
+            var spawnRotation = Quaternion.Euler(0f, Random.Range(0, 180f), 0f);
+
+            csSpawnPoint enemySpawnPoint = (Instantiate(spawnPoint, spawnPosition, spawnRotation) as GameObject).GetComponent<csSpawnPoint>();
+            enemySpawnPoints.Add(enemySpawnPoint);
+        }	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void SpawnEnemies(/* networking */)
+    {
+        int i = 0;
+
+        foreach(csSpawnPoint sp in enemySpawnPoints)
+        {
+            Vector3 position = sp.transform.position;
+            Quaternion rotation = sp.transform.rotation;
+            GameObject newEnemy = Instantiate(enemy, position, rotation) as GameObject;
+
+            newEnemy.name = i+"";
+            i++;
+
+            csPlayerController pc = newEnemy.GetComponent<csPlayerController>();
+            csHealth h = newEnemy.GetComponent<csHealth>();
+
+            h.currentHealth = 100;
+            h.OnChangeHealth();
+
+            h.isEnemy = true;
+            i++;
+        }
+    }
 }
