d32469c71fd2c17fa96a2f810842dffbcf91898b
HyunSeoChoi <arenaofjagal@naver.com>
2019-08-25 16:37:57 +0900

    Add PlayerMove and Bullet Shoot Script

--- Assets/Scripts/csHealth.cs
+++ Assets/Scripts/csHealth.cs
@@ -4,11 +4,55 @@ using UnityEngine;
 
 public class csHealth : MonoBehaviour {
 
+    public const int maxHealth = 100;
+    public bool destroyOnDeath;
+    public int currentHealth = maxHealth;
+    public bool isEnemy = false;
+
+    public RectTransform healthBar;
+
+    private bool isLocalPlayer;
 	// Use this for initialization
 	void Start () {
-		
+        csPlayerController pc = GetComponent<csPlayerController>();
+        isLocalPlayer = pc.isLocalPlayer;
 	}
 	
+    public void TakeDamage(GameObject playerFrom, int amount)
+    {
+        currentHealth -= amount;
+        // network
+    }
+
+    public void OnChangeHealth()
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+
+        if(currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHealth = maxHealth;
+                healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+                Respawn();
+            }
+        }
+    }
+
+    void Respawn()
+    {
+        if (isLocalPlayer)
+        {
+            Vector3 spawnPoint = Vector3.zero;
+            Quaternion spawnRotation = Quaternion.Euler(0, 180, 0);
+            transform.position = spawnPoint;
+            transform.rotation = spawnRotation;
+        }
+    }
 	// Update is called once per frame
 	void Update () {
 		
